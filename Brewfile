6510ab70c84e615bd1398716e8da96696c472b3b
Davi Naizer <davi.naizer@gmail.com>
2021-10-29 09:44:57 +0100

    feat: adds all main files and script logic

---
+++ Brewfile
@@ -0,0 +1,37 @@
+# 'brew install'
+brew 'git'
+brew 'gh'
+brew 'nvm'
+brew 'yarn'
+brew 'python'
+
+# 'brew tap'
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+
+# 'brew install --cask'
+#   IDEs and Dev tools
+cask 'beyond-compare'
+cask 'cyberduck'
+cask 'figma'
+cask 'imageoptim'
+cask 'sublime-text'
+cask 'visual-studio-code'
+cask "github"
+cask "iterm2"
+
+#   Browsers
+cask "firefox"
+cask "google-chrome"
+
+#   OS tools and Apps
+cask 'font-ibm-plex'
+cask 'rectangle'
+
+#   Misc apps
+cask 'fanny'
+cask 'grammarly'
+cask 'namechanger'
+cask 'google-drive'
+cask 'spotify'
+cask 'zoom'
---
+++ install.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+# author: Davi Naizer
+# created: 2021-10-18
+# updated: 2021-10-29
+
+# SCRIPT USAGE
+# LOCALLY
+# $ bash install.sh
+
+# LOAD CONSTANTS FILE
+source constants.env
+
+# UTILS FUNCTIONS
+function cecho() {
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"
+    YELLOW="\033[1;33m"
+    CYAN="\033[1;36m"
+    NC="\033[0m" # No Color
+
+    printf "${!1}${2} ${NC}\n"
+}
+
+function abort() {
+    printf "%s\n" "$@"
+    exit 1
+}
+
+function promptContinue() {
+    cecho "GREEN" "Do you want to continue (Y/n)?"
+    read answer
+    if [ "$answer" != "${answer#[Nn]}" ]; then
+        abort "Operation aborted!"
+    fi
+}
+
+function checkOS() {
+    OS="$(uname)"
+    if [[ "${OS}" == "Linux" ]]; then
+        OS_IS_LINUX=1
+    elif [[ "${OS}" != "Darwin" ]]; then
+        abort "This script is only supported on macOS and Linux."
+    fi
+}
+
+# MAIN FUNCTIONS
+function welcomeMessage() {
+    echo
+    cecho "YELLOW" "Welcome to the Dev Env Setup Script v${SCRIPT_VERSION}"
+    echo
+    echo "This script will create a workspace folder and install most applications and respositories required to work on as a FrontEnd Dev."
+    echo "It will take a while to complete, so go grab youself a coffee in the meantime."
+    echo
+    echo "The WORKSPACE folder will be created in the following location:"
+    cecho "CYAN" $WORKSPACE_PATH
+    echo
+
+    promptContinue
+}
+
+function installSystemDeps() {
+    echo
+    cecho "GREEN" "Installing Homebrew (Package manager)..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+function installAppsBundle() {
+    echo
+    cecho "GREEN" "Installing apps bundle (Brewfile)..."
+    brew bundle --verbose
+}
+
+function installNodeDeps() {
+    echo
+    cecho "GREEN" "Installing Node and global packages..."
+    nvm install 10
+    nvm install 12
+    nvm alias default 12
+}
+
+function createWorkspaceFolders() {
+    echo
+    cecho "GREEN" "Creating Workspace and Subfolders..."
+
+    for dir in "${WORKSPACE_SUBFOLDERS[@]}"; do
+        SUBFOLDER_PATH=${WORKSPACE_PATH}/${dir}
+        echo "Creating folder @ ${SUBFOLDER_PATH}"
+        [[ ! -d "$SUBFOLDER_PATH" ]] && mkdir -p "${SUBFOLDER_PATH}"
+    done
+}
+
+function cloneRepos() {
+    echo
+    cecho "GREEN" "Cloning GAMESYS-DESIGN repositories..."
+    for repo in "${GIT_REPO_LIST[@]}"; do
+        REPO_REMOTE_PATH="${GIT_REPO_URL}/${repo}"
+        REPO_LOCAL_PATH="${WORKSPACE_PATH}/gamesys-design/${repo}"
+        cecho "CYAN" "Cloning project: ${repo}"
+        git clone $REPO_REMOTE_PATH $REPO_LOCAL_PATH
+    done
+}
+
+function init() {
+    welcomeMessage
+
+    # START TIMER
+    startTime=$SECONDS
+
+    # INSTALL DEPENDENCIES
+    installSystemDeps
+    installAppsBundle
+    installNodeDeps
+
+    # create workspace and clone repos
+    createWorkspaceFolders
+    cloneRepos
+
+    # END TIMER
+    duration=$((SECONDS - startTime))
+    cecho "GREEN" "Installation completed in ${duration} seconds."
+}
+
+# RUN SCRIPT
+init
